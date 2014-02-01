0982f7a06ec3d5cfb0c1efac0ba03ad8dc1f0284
badp <badpazzword@gmail.com>
2014-02-01 11:36:33 +0100

    chatwrapper: Do not simply cross your fingers when sending messages
    * Sleep 5 seconds between messages
    * When told to wait x seconds, wait 1.5 times as much
      (and then still 5 seconds on success)
    * Avoid sending the same message more than once.

--- SEChatWrapper.py
+++ SEChatWrapper.py
@@ -1,4 +1,8 @@
 import SEChatBrowser
+import re
+import time
+
+TOO_FAST_RE = "You can perform this action again in (\d+) seconds"
 
 
 class SEChatWrapper(object):
@@ -6,6 +10,7 @@ class SEChatWrapper(object):
   def __init__(self, site="SE"):
     self.br = SEChatBrowser.SEChatBrowser()
     self.site = site
+    self.previous = None
 
   def login(self, username, password):
     self.br.loginSEOpenID(username, password)
@@ -18,5 +23,27 @@ class SEChatWrapper(object):
       self.br.loginMSO()
 
   def sendMessage(self, room, text):
-    return self.br.postSomething("/chats/"+room+"/messages/new",
-                                 {"text": text})
+    sent = False
+    if text == self.previous:
+      text = " " + text
+    while not sent:
+      wait = 0
+      response = self.br.postSomething("/chats/"+room+"/messages/new",
+                                       {"text": text})
+      if isinstance(response, str): # Whoops, too fast.
+        match = re.match(TOO_FAST_RE, response)
+        if match:
+          wait = int(match.group(1)) * 1.5
+      elif isinstance(response, dict):
+        if response["id"] is None: # Duplicate message?
+          text = text + " " # Let's not risk turning the message
+          wait = 1          # into a codeblock accidentally.
+
+      if wait:
+        print "Waiting %.1f seconds" % wait
+        time.sleep(wait)
+      else:
+        sent = True
+        self.previous = text
+    time.sleep(5)
+    return response
