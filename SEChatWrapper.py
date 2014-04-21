564e55f47124cb3e63edbf8eb1585489e0499486
Manish Goregaokar <manishsmail@gmail.com>
2014-04-22 04:45:26 +0530

    +MSE

--- SEChatWrapper.py
+++ SEChatWrapper.py
@@ -48,6 +48,8 @@ class SEChatWrapper(object):
       self.br.loginSO()
     elif self.site == "MSO":
       self.br.loginMSO()
+    elif self.site == "MSE":
+      self.br.loginMSE()
     else:
       raise ValueError("Unable to login to site: %r" % (self.site,))
 
