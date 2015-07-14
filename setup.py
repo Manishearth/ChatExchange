f8bb2ee5e5e5b42a378b256921a77089ffa2e183
ByteCommander <ByteCommander@users.noreply.github.com>
2015-07-14 16:36:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,15 @@ import setuptools
 
 
 setuptools.setup(
-    name='ChatExchange',
-    version='0.0.1a2dev2',
-    url='https://github.com/Manishearth/ChatExchange',
+    name='ChatExchange3',
+    version='0.0.1',
+    url='https://github.com/ByteCommander/ChatExchange3',
     packages=[
-        'chatexchange'
+        'chatexchange3'
     ],
     install_requires=[
-        'beautifulsoup4==4.3.2',
-        'requests==2.2.1',
-        'websocket-client==0.13.0',
-        # only for dev:
-        'coverage==3.7.1',
-        'epydoc==3.0.1',
-        'httmock==1.2.2',
-        'pytest-capturelog==0.7',
-        'pytest-timeout==0.3',
-        'pytest==2.5.2',
+        'beautifulsoup4>=4.3.2',
+        'requests>=2.2.1',
+        'websocket-client>=0.13.0'
     ]
 )
