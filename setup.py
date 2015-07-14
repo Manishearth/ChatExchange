e1a57ac1ab9242791feb65aa669a570d3370096c
ByteCommander <ByteCommander@users.noreply.github.com>
2015-07-14 16:57:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     install_requires=[
         'beautifulsoup4>=4.3.2',
         'requests>=2.2.1',
-        'websocket-client>=0.13.0'
+        'websocket-client>=0.13.0',
         # only for dev:
         'coverage==3.7.1',
         'epydoc==3.0.1',
