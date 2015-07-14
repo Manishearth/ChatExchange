098339c8be2e61489179b564f751c7e63b6c73e4
ByteCommander <ByteCommander@users.noreply.github.com>
2015-07-14 18:51:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ setuptools.setup(
         'requests>=2.2.1',
         'websocket-client>=0.13.0',
         # only for dev:
-        'coverage==3.7.1',
-        'epydoc==3.0.1',
-        'httmock==1.2.2',
-        'pytest-capturelog==0.7',
-        'pytest-timeout==0.3',
-        'pytest==2.5.2'
+        'coverage>=3.7.1',
+        'epydoc>=3.0.1',
+        'httmock>=1.2.2',
+        'pytest-capturelog>=0.7',
+        'pytest-timeout>=0.3',
+        'pytest>=2.5.2'
     ]
 )
