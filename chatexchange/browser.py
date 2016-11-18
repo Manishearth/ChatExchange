ecb54b129210dfbbf6c1846efec4ce57c3f1d624
Ralph Embree <ralph.embree@brominator.org>
2016-11-18 19:41:28 +0000

    Fix bug when logging in
    The user ID is retrieved by parsing the user URL.  When using / as the
    delimiter, the user ID is at position 2 if the URL is of the form
    stackoverflow.com/user/0000/username, but will be at position 4 if the
    URL is of the form https://stackoverflow.com/user/0000/username.
    To fix this problem, find the ID from the end rather than from the
    beginning.  My previous patch didn't account for the username at the
    end of the URL, so please ignore that patch.

--- chatexchange/browser.py
+++ chatexchange/browser.py
@@ -409,8 +409,8 @@ class Browser(object):
             'transcript/message/%s' % (message_id,))
 
         room_soups = transcript_soup.select('.room-name a')
-        room_soup = room_soups[-1]
-        room_id = int(room_soup['href'].split('/')[2])
+        room_soup = room_soups[]
+        room_id = int(room_soup['href'].split('/')[-2])
         room_name = room_soup.text
 
         messages_data = []
