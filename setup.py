9c1b973e611439d18e455783ced569143a4f260a
tripleee <tripleee@users.noreply.github.com>
2022-04-20 08:43:15 +0300

    setup.py: move dev dependencies to extras_require
    Installing the package should not pull in complex development requirements by default

--- setup.py
+++ setup.py
@@ -10,14 +10,17 @@ setuptools.setup(
     install_requires=[
         'beautifulsoup4>=4.3.2',
         'requests>=2.2.1',
-        'websocket-client>=0.13.0',
-        # only for dev:
-        'coverage>=4.5.0',
-        'epydoc>=3.0.1',
-        'httmock>=1.2.2',
-        'pytest-timeout>=0.3',
-        'pytest>=3.4.2',
-        'py>=1.5.0',
-        'bpython>=0.16'
-    ]
+        'websocket-client>=0.13.0'
+    ],
+    extras_require={
+        'dev': [
+            'coverage>=4.5.0',
+            'epydoc>=3.0.1',
+            'httmock>=1.2.2',
+            'pytest-timeout>=0.3',
+            'pytest>=3.4.2',
+            'py>=1.5.0',
+            'bpython>=0.16'
+        ]
+    }
 )
